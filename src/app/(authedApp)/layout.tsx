d10f03d198ad4f917f105ec3c45719c7e562b6c3
vitali.auseikou <vitali.auseikou@indev.by>
2024-11-12 10:50:51 +0300

    fix

--- src/app/(authedApp)/layout.tsx
+++ src/app/(authedApp)/layout.tsx
@@ -10,6 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import type { ReactNode } from 'react'
 
 import { ToastContainer } from 'react-toastify'
+import Head from "next/head";
 
 const MontserratSans = Montserrat({
   subsets: ['latin'],
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       lang='en'
       className={MontserratSans.className}
     >
+    <Head> <script type="text/javascript"> {` (function(m,e,t,r,i,k,a){m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) }; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } } k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a) })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(98887573, "init", { clickmap: true, trackLinks: true, accurateTrackBounce: true }); `} </script> <noscript> <div> <img src="https://mc.yandex.ru/watch/98887573" style={{ position: "absolute", left: "-9999px" }} alt="" /> </div> </noscript> </Head>
       <body
         suppressHydrationWarning={true}
         className='flex min-h-screen flex-col'
